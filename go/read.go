ca675298ba9f7c99c6efc0679a9e5eda13dcc9ac
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-21 14:13:42 -0500

    reading RingN

---
+++ go/read.go
@@ -0,0 +1,16 @@
+package ringN
+
+func (c *Circular) Read(buf []byte) (n int) {
+  for n = 0; c.N < len(c.buf) && n < len(buf); n++ {
+    end := c.end
+    if  end == len(c.buf) {
+     end = 0
+    }
+    c.buf[end] = buf[n]
+    end++
+
+    c.end = end
+    c.N++
+  }
+  return n
+}
