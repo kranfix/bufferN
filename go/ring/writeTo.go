7d8b0bd3f1143a3b584126d6091cd8b4eea3bc38
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-22 18:10:38 -0500

    go.ring: WriteTo tested

--- go/ring/writeTo.go
+++ go/ring/writeTo.go
@@ -3,18 +3,36 @@ package ringN
 import "io"
 
 func (r *Ring) WriteTo(w io.Writer) (n int64, err error) {
-  N := r.Len()
-  s := make([]byte,n)
-  N,err = r.Read(s)
-  if err != nil {
-    return
+  n = int64(0)
+  off := r.off
+  end := r.end
+  L := r.Len()
+  N := 0
+
+  if off >= end && L > 0 {
+    N,err = w.Write(r.buf[off:])
+    n = int64(N)
+    off += N
+
+    r.N -= N
+    //fmt.Printf("from 1: end=%d off=%d\n",end,off)
+    if off == r.Cap() && end != off {
+      off = 0
+    }
+    r.off = off
+
+    if err != nil {
+      return
+    }
   }
 
-  N,err = w.Write(s)
-  if err != nil {
-    return
-  }
 
-  n := int64(N)
+  if off == r.Cap() && end == off && r.N > 0 {
+    off = 0
+  }
+  N,err = w.Write(r.buf[off:end])
+  n += int64(N)
+  r.N -= N
+  r.off = off + N
   return
 }
